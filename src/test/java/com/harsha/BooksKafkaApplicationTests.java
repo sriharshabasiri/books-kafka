3bd304b371624ba075c82dac28284bcb921b7a49
sriharshabasiri <41380227+sriharshabasiri@users.noreply.github.com>
2024-11-12 12:53:17 -0700

    Update BooksKafkaApplicationTests.java

--- src/test/java/com/harsha/BooksKafkaApplicationTests.java
+++ src/test/java/com/harsha/BooksKafkaApplicationTests.java
@@ -11,3 +11,32 @@ class BooksKafkaApplicationTests {
 	}
 
 }
+
+
+
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="25" height="25">
+  <g fill="none" stroke="black" stroke-width="2">
+    <ellipse cx="32" cy="16" rx="20" ry="6" fill="grey"/>
+    <ellipse cx="32" cy="32" rx="20" ry="6" fill="grey"/>
+    <ellipse cx="32" cy="48" rx="20" ry="6" fill="grey"/>
+    <line x1="12" y1="16" x2="12" y2="32" />
+    <line x1="52" y1="16" x2="52" y2="32" />
+    <line x1="12" y1="32" x2="12" y2="48" />
+    <line x1="52" y1="32" x2="52" y2="48" />
+  </g>
+</svg>
+
+
+
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="25" height="25">
+  <g fill="none" stroke="black" stroke-width="2">
+    <ellipse cx="32" cy="16" rx="20" ry="6" />
+    <ellipse cx="32" cy="32" rx="20" ry="6" fill="grey"/>
+    <ellipse cx="32" cy="48" rx="20" ry="6" fill="grey"/>
+    <line x1="12" y1="16" x2="12" y2="32" />
+    <line x1="52" y1="16" x2="52" y2="32" />
+    <line x1="12" y1="32" x2="12" y2="48" />
+    <line x1="52" y1="32" x2="52" y2="48" />
+  </g>
+</svg>
